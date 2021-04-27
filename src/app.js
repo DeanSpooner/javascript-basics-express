174c07cb07bd79b3fbde2d2a5ceae7aec2680536
Dean Spooner <dean.spooner@me.com>
2021-04-27 23:23:36 +0100

    Completed numbers tests up to end of subtract.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const {
   firstCharacters,
 } = require('./lib/strings');
 
+const { add, subtract } = require('./lib/numbers');
+
 const app = express();
+
+// STRINGS
 
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
@@ -28,6 +32,37 @@ app.get('/strings/first-characters/:string', (req, res) => {
   } else {
     res.json({ result: firstCharacter(req.params.string) });
   }
+});
+
+// NUMBERS
+
+// app.get('/numbers/add/:first/and/:second', (req, res) => {
+//   if (
+//     typeof parseInt(req.params.first, 10) === 'number' &&
+//     typeof parseInt(req.params.second, 10) === 'number'
+//   ) {
+//     res.json({ result: add(parseInt(req.params.first, 10), parseInt(req.params.second, 10)) });
+//   } else {
+//     res.sendStatus(400);
+//   }
+// });
+
+app.get('/numbers/add/:first/and/:second', (req, res) => {
+  const first = parseInt(req.params.first, 10);
+  const second = parseInt(req.params.second, 10);
+
+  return Number.isNaN(first) || Number.isNaN(second)
+    ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
+    : res.json({ result: add(first, second) });
+});
+
+app.get('/numbers/subtract/:first/from/:second', (req, res) => {
+  const first = parseInt(req.params.first, 10);
+  const second = parseInt(req.params.second, 10);
+
+  return Number.isNaN(first) || Number.isNaN(second)
+    ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
+    : res.json({ result: subtract(second, first) });
 });
 
 module.exports = app;

1eeaf32dd3c52e87214e8051cc3964175aa83829
Dean Spooner <dean.spooner@me.com>
2021-05-01 14:36:46 +0100

    Completed number tests.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const app = express();
+
+app.use(express.json());
+
 const {
   sayHello,
   uppercase,
@@ -8,9 +12,9 @@ const {
   firstCharacters,
 } = require('./lib/strings');
 
-const { add, subtract } = require('./lib/numbers');
+const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
 
-const app = express();
+const { negate } = require('./lib/booleans');
 
 // STRINGS
 
@@ -63,6 +67,76 @@ app.get('/numbers/subtract/:first/from/:second', (req, res) => {
   return Number.isNaN(first) || Number.isNaN(second)
     ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
     : res.json({ result: subtract(second, first) });
+});
+
+app.get('/numbers/subtract/:first/from/:second', (req, res) => {
+  const first = parseInt(req.params.first, 10);
+  const second = parseInt(req.params.second, 10);
+
+  return Number.isNaN(first) || Number.isNaN(second)
+    ? res.status(400).json({ error: 'Parameters must be valid numbers.' })
+    : res.json({ result: subtract(second, first) });
+});
+
+app.post('/numbers/multiply', (req, res) => {
+  if (!req.body.a || !req.body.b) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  }
+  if (isNaN(req.body.a) || isNaN(req.body.b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: multiply(req.body.a, req.body.b) });
+  }
+});
+
+// app.post('/numbers/divide', (req, res) => {
+//   if (!req.body.a || !req.body.b) {
+//     res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+//   } else if (isNaN(req.body.a) || (isNaN(req.body.b) && req.body.b !== 0)) {
+//     res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+//   } else if (req.body.b === 0) {
+//     res.status(400).json({ error: 'Unable to divide by 0.' });
+//   } else {
+//     res.status(200).json({ result: divide(req.body.a, req.body.b) });
+//   }
+// });
+
+app.post('/numbers/divide', (req, res) => {
+  if (req.body.a == null || req.body.b == null) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = Number(req.body.a);
+  const b = Number(req.body.b);
+
+  if (b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: divide(a, b) });
+  }
+});
+
+app.post('/numbers/remainder', (req, res) => {
+  if (req.body.a == null || req.body.b == null) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  }
+
+  const a = Number(req.body.a);
+  const b = Number(req.body.b);
+
+  if (b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (isNaN(a) || isNaN(b)) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: remainder(a, b) });
+  }
+});
+
+app.post('/booleans/negate', (req, res) => {
+  res.status(200).json({ result: negate(req.body.a) });
 });
 
 module.exports = app;
--- src/lib/booleans.js
+++ src/lib/booleans.js
@@ -21,83 +21,83 @@ const one = (a, b) => {
 const truthiness = a => {
   if (a) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const isEqual = (a, b) => {
   if (a === b) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const isGreaterThan = (a, b) => {
   if (a > b) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const isLessThanOrEqualTo = (a, b) => {
   if (a <= b) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const isOdd = a => {
   if (a % 2 === 1) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const isEven = a => {
   if (a % 2 === 0) {
     return true;
-  } else {
-    return false;
-  };
-}
+  }
+  return false;
+};
 
 const isSquare = a => {
-  if (Math.sqrt(a) === Math.trunc(Math.sqrt(a))) { //Here, only square-roots of square numbers would have no decimal, so the square root's truncated form would be identical.
+  if (Math.sqrt(a) === Math.trunc(Math.sqrt(a))) {
+    // Here, only square-roots of square numbers would have no decimal, so the square root's truncated form would be identical.
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const startsWith = (char, string) => {
   if (char === string.charAt(0)) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const containsVowels = string => {
-  if (string.includes("a") || string.includes("A") || string.includes("e") || string.includes("E") || 
-  string.includes("i") || string.includes("I") || string.includes("o") || string.includes("O") || 
-  string.includes("u") || string.includes("U")) {
+  if (
+    string.includes('a') ||
+    string.includes('A') ||
+    string.includes('e') ||
+    string.includes('E') ||
+    string.includes('i') ||
+    string.includes('I') ||
+    string.includes('o') ||
+    string.includes('O') ||
+    string.includes('u') ||
+    string.includes('U')
+  ) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 const isLowerCase = string => {
   if (string === string.toLowerCase()) {
     return true;
-  } else {
-    return false;
-  };
+  }
+  return false;
 };
 
 module.exports = {
@@ -115,5 +115,5 @@ module.exports = {
   isSquare,
   startsWith,
   containsVowels,
-  isLowerCase
+  isLowerCase,
 };
